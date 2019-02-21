32629cba3db4924dac8a4e5827bca29bd5d3b554
Sarah Dahnke <sarahrosedahnke@gmail.com>
2019-02-21 17:46:31 -0500

    adding app code

---
+++ 07_feb21_day7/app/app.js
@@ -0,0 +1,5 @@
+//console.log("hellooooo world")
+
+const greeter = require('./greeter.js');
+
+greeter.sayHelloinSpanish();
---
+++ 07_feb21_day7/app/greeter.js
@@ -0,0 +1,15 @@
+//greet in Spanish
+
+//greet in English
+
+module.exports = {
+	sayHelloinSpanish: function() {
+	console.log("hola")
+	},
+	sayHelloinEnglish: function() {
+	console.log("hello")
+	}
+};
+
+
+
